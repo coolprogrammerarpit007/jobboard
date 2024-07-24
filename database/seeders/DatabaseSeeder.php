a95829411df82af5bf6952fc9312cd6869e645f0
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-24 13:52:44 +0530

    commit dummy data with seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(10)->create(); // will create the 10 dummy users
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Listing::create([
+            'title' => 'Laravel Senior Developer',
+            'tags' => 'JavaScript,Python,PHP',
+            'company' => 'Acme Corp',
+            'location' => 'Boston, USA',
+            'email' => 'email@gmail.com',
+            'website' => 'www.acme.com',
+            'description' => 'Our Company requires an Experienced and Passionate Software Engineer!'
+
+        ]);
     }
 }
