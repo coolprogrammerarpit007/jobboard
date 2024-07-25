eed65bcd76e077077a8510927e30d4fdecc58acc
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-25 15:42:06 +0530

    tags component worked

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -6,41 +6,28 @@
     </a>
     <div class="mx-4">
         <div class="bg-gray-50 border border-gray-200 p-10 rounded">
-            <div
-                class="flex flex-col items-center justify-center text-center"
-            >
-                <img
-                    class="w-48 mr-6 mb-6"
-                    src="{{asset('images/no-image.png')}}"
-                    alt=""
-                />
+            <div class="flex flex-col items-center justify-center text-center">
+                <img class="w-48 mr-6 mb-6" src="{{ asset('images/no-image.png') }}" alt="" />
 
-                <h3 class="text-2xl mb-2">{{$listing->title}}</h3>
-                <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
-                <ul class="flex">
-                    <li
-                        class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-                    >
+                <h3 class="text-2xl mb-2">{{ $listing->title }}</h3>
+                <div class="text-xl font-bold mb-4">{{ $listing->company }}</div>
+                {{-- <ul class="flex">
+                    <li class="bg-black text-white rounded-xl px-3 py-1 mr-2">
                         <a href="#">Laravel</a>
                     </li>
-                    <li
-                        class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-                    >
+                    <li class="bg-black text-white rounded-xl px-3 py-1 mr-2">
                         <a href="#">API</a>
                     </li>
-                    <li
-                        class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-                    >
+                    <li class="bg-black text-white rounded-xl px-3 py-1 mr-2">
                         <a href="#">Backend</a>
                     </li>
-                    <li
-                        class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-                    >
+                    <li class="bg-black text-white rounded-xl px-3 py-1 mr-2">
                         <a href="#">Vue</a>
                     </li>
-                </ul>
+                </ul> --}}
+                <x-listing-tag :tagsCsv="$listing->tags" />
                 <div class="text-lg my-4">
-                    <i class="fa-solid fa-location-dot"></i>{{$listing->location}}
+                    <i class="fa-solid fa-location-dot"></i>{{ $listing->location }}
                 </div>
                 <div class="border border-gray-200 w-full mb-6"></div>
                 <div>
@@ -49,24 +36,19 @@
                     </h3>
                     <div class="text-lg space-y-6">
                         <p>
-                            {{$listing->description}}
+                            {{ $listing->description }}
                         </p>
                         <p>
-                            {{$listing->description}}
+                            {{ $listing->description }}
                         </p>
 
-                        <a
-                            href="mailto:{{$listing->email}}"
-                            class="block bg-laravel text-white mt-6 py-2 rounded-xl hover:opacity-80"
-                            ><i class="fa-solid fa-envelope"></i> {{$listing->company}}</a
-                        >
+                        <a href="mailto:{{ $listing->email }}"
+                            class="block bg-laravel text-white mt-6 py-2 rounded-xl hover:opacity-80"><i
+                                class="fa-solid fa-envelope"></i> {{ $listing->company }}</a>
 
-                        <a
-                            href="https://test.com"
-                            target="_blank"
-                            class="block bg-black text-white py-2 rounded-xl hover:opacity-80"
-                            ><i class="fa-solid fa-globe"></i>{{$listing->website}}</a
-                        >
+                        <a href="https://test.com" target="_blank"
+                            class="block bg-black text-white py-2 rounded-xl hover:opacity-80"><i
+                                class="fa-solid fa-globe"></i>{{ $listing->website }}</a>
                     </div>
                 </div>
             </div>
