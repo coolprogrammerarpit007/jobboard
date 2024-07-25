5a22c8e82ad55f83fe6ab7c10da304b6b32ec318
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-25 12:02:07 +0530

    built the index page

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -15,7 +15,7 @@
                 @foreach ($listings as $listing)
                     <div class="bg-gray-50 border border-gray-200 rounded p-6">
                         <div class="flex">
-                            <img class="hidden w-48 mr-6 md:block" src="images/acme.png" alt="" width="100" />
+                            <img class="hidden w-48 mr-6 md:block" src="{{asset('images/no-image.png')}}" alt="laravel" width="100" />
                             <div>
                                 <h3 class="text-2xl">
                                     <a href="show.html">{{$listing->title}}</a>
